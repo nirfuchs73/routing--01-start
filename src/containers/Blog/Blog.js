0aa951637d96d1798dc5b2a19ccff47a04a5737d
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-08-19 19:06:38 +0300

    Update

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -18,7 +18,11 @@ class Blog extends Component {
           <nav>
             <ul>
               <li><Link to="/">Home</Link></li>
-              <li><Link to="/new-post">New Post</Link></li>
+              <li><Link to={{
+                pathname: '/new-post',
+                hash: '#submit',
+                search: '?quick-submit=true'
+              }}>New Post</Link></li>
             </ul>
           </nav>
         </header>
